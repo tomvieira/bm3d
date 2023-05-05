2c1547dc53144fc7129b284bda06a8b7810ddd5c
Wellington Vieira <tomvieira@outlook.com>
2023-05-05 14:32:11 -0300

    bm3d algoritmo

--- bm3d-analise.py
+++ bm3d-analise.py
@@ -1,16 +1,14 @@
-from skimage import io, img_as_float
+from skimage import io, img_as_float, img_as_uint
 import bm3d
 import cv2 as cv
 import numpy as np
+img = io.imread('ruim.png', as_gray=True)
+img_ruim = np.array(img.copy(), dtype=float)
 
-img_ruim = img_as_float(io.imread('ruim.png', as_gray=True))
-
-dm3d_ruido = bm3d.bm3d(img_ruim, sigma_psd=0.2,
-                       stage_arg=bm3d.BM3DStages.ALL_STAGES)
-cv.imshow('img_ruim', img_ruim)
-cv.imshow('dm3d_ruido_clean', dm3d_ruido)
-dm3d_ruido = dm3d_ruido.astype(np.uint8)
-cv.imwrite('boa.png', dm3d_ruido)
+img_sem_ruido = bm3d.bm3d(img_ruim, sigma_psd=0.2,
+                          stage_arg=bm3d.BM3DStages.ALL_STAGES)
+io.imsave('boa.png', img_sem_ruido)
+cv.imshow('Ruim', img_ruim)
+cv.imshow('Sem ruido', img_sem_ruido)
 cv.waitKey(0)
 cv.destroyAllWindows()
-
